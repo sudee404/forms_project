ba60e41d13d72c80f412a7928e32000db200ea17
sudee404 <sudaise18@gmail.com>
2022-01-24 16:51:29 +0300

    Update ALLOWED_HOSTS with site and development server URL

--- forms_project/settings.py
+++ forms_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stormy-badlands-17123.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
